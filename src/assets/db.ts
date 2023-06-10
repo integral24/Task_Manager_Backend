c5e38a2d4df4871d176d4fb1d8782ea774a49333
Gowart <gowart24@gmail.com>
2023-06-10 16:36:58 +0300

    fix

--- src/assets/db.ts
+++ src/assets/db.ts
@@ -2,20 +2,20 @@ import mysql2 from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// const pool = mysql2.createPool({
-//   user: process.env.USER_DB,
-//   password: process.env.PASS,
-//   host: process.env.HOST,
-//   port: 3306,
-//   database: process.env.NAME_DB
-// });
-
 const pool = mysql2.createPool({
-  user: process.env.USER_L,
-  password: process.env.PASS_L,
-  host: process.env.HOST_L,
+  user: process.env.USER_DB,
+  password: process.env.PASS,
+  host: process.env.HOST,
   port: 3306,
-  database: process.env.NAME_DB_L
+  database: process.env.NAME_DB
 });
+
+// const pool = mysql2.createPool({
+//   user: process.env.USER_L,
+//   password: process.env.PASS_L,
+//   host: process.env.HOST_L,
+//   port: 3306,
+//   database: process.env.NAME_DB_L
+// });
 
 export default pool;
